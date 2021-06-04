2fcd53daefa3d234734ee5907855915dc024b55a
ayush raj <ayushthebest001@gmail.com>
2021-06-05 04:13:45 +0530

    user registering to db

--- frontend/src/components/auth/signup.js
+++ frontend/src/components/auth/signup.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import {useState} from 'react';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { useState } from "react";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -33,16 +33,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -55,75 +55,88 @@ export default function SignIn() {
 
   //this.handleEmailChange = this.handleEmailChange.bind(this);
 
-  const [passone, setPassone]= useState("")
-  const [passtwo, setPasstwo]= useState("")
+  const [passone, setPassone] = useState("");
+  const [passtwo, setPasstwo] = useState("");
 
-  const [firstname, setfirstname]= useState("")
-  const [lastname, setLastname]= useState("")
-  const [usertname, setUsername]= useState("")
-  const [email, setEmail]= useState("")
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
   function handleFirstname(e) {
     let val = e.target.value;
-    setfirstname(val)
-}
-function handleLastname(e) {
+    setfirstname(val);
+  }
+  function handleLastname(e) {
     let val = e.target.value;
-    setLastname(val)
-}
+    setLastname(val);
+  }
 
-function handleUsername(e) {
+  function handleUsername(e) {
     let val = e.target.value;
-    setUsername(val)
-}
-  
+    setUsername(val);
+  }
 
-  
   function handleEmailChange(e) {
-        let val = e.target.value;
-        setPassone(val)
-    }
+    let val = e.target.value;
+    setPassone(val);
+  }
 
-    
   function handleEmailChange2(e) {
     let val = e.target.value;
-    setPasstwo(val)
-}
+    setPasstwo(val);
+  }
 
-function handleEmail(e) {
+  function handleEmail(e) {
     let val = e.target.value;
-    setEmail(val)
-}
+    setEmail(val);
+  }
 
-function validate(e) {
-
-    e.preventDefault()
-    // fetch("http://localhost:8000/login")
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data.message));
-
+  async function validate(e) {
+    e.preventDefault();
 
     var valid = false;
 
     // if (passwords match) and if (fields are not -> "") => valid = true
 
-    if(firstname === "" ||  lastname === "" || username ==="" || email === ""){
-        
-    }else{
-        if(passone === passtwo){
-            valid = true;
-        }
+    if (
+      firstname === "" ||
+      lastname === "" ||
+      username === "" ||
+      email === ""
+    ) {
+    } else {
+      if (passone === passtwo) {
+        valid = true;
+      }
     }
 
-    if (valid){
-        console.log('succsss')
-    } else  {
-        console.log("not allowed")
-            alert("Form has errors.")
-         
+    if (valid) {
+      var mg_body = JSON.stringify({
+        username: username,
+        first_name: firstname,
+        last_name: lastname,
+        email: email,
+        password: passtwo,
+      });
+      await fetch("http://localhost:8000/signup/user", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: mg_body,
+      })
+        .then((res) => {
+          console.log(res);
+          res.json();
+        })
+        .then((data) => console.log("================", data))
+        .catch((e) => {
+          //show alert ERROR
+        });
+    } else {
+      console.log("not allowed");
+      alert("Form has errors.");
     }
-}
-
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -136,7 +149,7 @@ function validate(e) {
           Sign Up
         </Typography>
         <form className={classes.form} noValidate>
-        <TextField
+          <TextField
             variant="outlined"
             margin="normal"
             required
@@ -148,7 +161,7 @@ function validate(e) {
             autoFocus
             onChange={handleUsername}
           />
-        <TextField
+          <TextField
             variant="outlined"
             margin="normal"
             required
@@ -172,7 +185,7 @@ function validate(e) {
             autoFocus
             onChange={handleLastname}
           />
-          
+
           <TextField
             variant="outlined"
             margin="normal"
@@ -209,7 +222,7 @@ function validate(e) {
             autoComplete="current-password"
             onChange={handleEmailChange2}
           />
-          
+
           <Button
             onClick={validate}
             type="submit"
@@ -220,7 +233,6 @@ function validate(e) {
           >
             Sign Up
           </Button>
-         
         </form>
       </div>
       <Box mt={8}>
